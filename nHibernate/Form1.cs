3f0f4a91f67264fd643ae24f5f0d69011d4eb005
Priya <Priyadarshini.professional@gmail.com>
2018-02-06 12:38:42 +0530

    First commit

---
+++ nHibernate/Form1.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using NHibernate;
+using NHibernate.Cfg;
+
+
+
+namespace nHibernate
+{
+    public partial class Form1 : Form
+    {
+        private Configuration  myConfiguration;
+        private ISessionFactory  mySessionFactory;
+        private ISession mySession;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            myConfiguration = new Configuration();
+            myConfiguration.Configure();
+            mySessionFactory = myConfiguration.BuildSessionFactory();
+            mySession = mySessionFactory.OpenSession();
+
+            //Add Record
+            using (mySession.BeginTransaction())
+            {
+                Contact loContact = new Contact {FirstName="John", LastName = "Doe" };
+                mySession.Save(loContact);
+                mySession.Transaction.Commit();   
+            }
+
+            //List Contact
+            ICriteria critera = mySession.CreateCriteria<Contact>();
+            IList<Contact> list = critera.List<Contact>();
+            mySession.Transaction.Commit();
+        }
+    }
+} 
